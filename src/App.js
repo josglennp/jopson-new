eddae7442e7f6f9c48615926330aa9f469fecb66
jos glenn pontejos <josglennpontejos@joss-MacBook-Pro.local>
2023-02-09 22:15:48 +0800

    new changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
-import Homepage from './components/Home/Homepage';
-import LoginPage from './components/LoginPage'
-import MainPage from './components/MainPage'
-import NotFound from './components/NotFound';
-import ForgotPassword from './components/admin/ForgotPassword';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./components/Home/Homepage";
+import LoginPage from "./components/LoginPage";
+import MainPage from "./components/MainPage";
+import ForgotPassword from "./components/admin/ForgotPassword";
 
+import "./App.css";
+import "./index.css";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={<Homepage />}  />
-        <Route path="/admin" exact element={<LoginPage />} />
+        <Route path="/" exact element={<Homepage />} />
+        {/* <Route path="/admin" exact element={<LoginPage />} />
         <Route path="/admin/dashboard" element={<MainPage />} />
-        <Route path='*' element={<NotFound />} />
-        <Route path="admin/forgotpass" element={<ForgotPassword />} />
+        <Route path="admin/forgotpass" element={<ForgotPassword />} /> */}
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -1,67 +1,105 @@
-import { Button, Typography } from '@mui/material';
-import React from 'react';
-import BgImage from '../../assets/images/bg.jpeg'
-import gearGif from '../../assets/images/gear.gif'
-import JopsonLogo from '../../assets/images/jopson-logo.png'
-import './style.css'
+import { Button, Typography } from "@mui/material";
+import React from "react";
+import BgImage from "../../assets/images/bg.jpeg";
+import gearGif from "../../assets/images/gear.gif";
+import JopsonLogo from "../../assets/images/jopson-logo.png";
+import "./style.css";
+import Navbar from "../navbar/Navbar";
+import manLift from "../../assets/images/manLift.jpg";
+
 function Homepage(props) {
-    return (
-        <div style={styles.padding}>
-            <div className='bg-container'>
-                <div style={styles.gearContainer}>
-                    <img alt="gear" src={gearGif}  style={styles.gear}/>
-                </div>
-                <div style={styles.titleContainer}>
-                    <img alt='logo' src={JopsonLogo}  style={styles.logo}/>
-                    <Typography variant='h2' style={styles.homeName}> Jop-Son Enterprises </Typography>
-                    <Button size='large' variant='outlined' style={styles.homeButton}>Get Started</Button>
-                </div>
-            </div>
-                <img alt='bg' src={BgImage} className="bg" />
+  return (
+    <>
+      <div className="gradient__bg">
+        <Navbar />
+        {/* <div style={styles.padding}>
+        <div className="bg-container">
+        <div style={styles.gearContainer}>
+        <img alt="gear" src={gearGif} style={styles.gear} />
         </div>
-    );
+        <div style={styles.titleContainer}>
+        <img alt="logo" src={JopsonLogo} style={styles.logo} />
+        <Typography variant="h2" style={styles.homeName}>
+        {" "}
+        Jop-Son Enterprises{" "}
+        </Typography>
+        <Button size="large" variant="outlined" style={styles.homeButton}>
+        Get Started
+        </Button>
+        </div>
+        </div>
+        <img alt="bg" src={BgImage} className="bg" />
+      </div> */}
+        <div className="gpt3__header section_padding" id="home">
+          <div className="gpt3__header-content">
+            <h1 className="gradient__text">
+              We Build Your Dreams Come True. Let's Make Something Amazing!
+            </h1>
+            <p className="gradient__text">
+              Yet bed any for travelling assistance undulgence unpleasing. Not
+              all thoughts all excercise blessing. Indulgence way everything joy
+              alteration boisterous the attatchment. Party we years to allow
+              asked of.
+            </p>
+
+            <div className="gpt3__header-content__input">
+              <input type="email" placeholder="YOUR EMAIL ADDRESS" />
+              <button type="button">Get Started</button>
+            </div>
+
+            <div className="gpt3__header-content__people">
+              <img src={gearGif} alt="people" />
+              <p>
+                500+ Projects We Have Built In The Past 12 years of Experience
+              </p>
+            </div>
+          </div>
+          <div className="gpt3__header-image">
+            <img src={manLift} alt="Man-Lift" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Homepage;
 
-
 const styles = {
-    container: { 
-        padding: 0
-    },
-    homeName: {
-        padding: 2,
-        fontWeight: '350'
-    },
-    homeButton: {
-        margin: 10,
-        fontSize: '30px',
-        borderRadius: '10px'
-    },
-    gearContainer: {
-        position: 'absolute',
-        marginTop: '5%',
-        alignItems: 'right',
-        // border: '1px solid green',
-        width: '100%',
-        paddingLeft : '70%'
-    },
-    gear: {
-        height: '100%',
-        width: '15%'
-    },
-    logo: {
-        height: '30%',
-        width: '30%'
-    },
-    titleContainer: {
-        display:'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        border: '1px solid green',
-        height: '100%',
-
-    }
-
-}
+  container: {
+    padding: 0,
+  },
+  homeName: {
+    padding: 2,
+    fontWeight: "350",
+  },
+  homeButton: {
+    margin: 10,
+    fontSize: "30px",
+    borderRadius: "10px",
+  },
+  gearContainer: {
+    position: "absolute",
+    marginTop: "5%",
+    alignItems: "right",
+    // border: '1px solid green',
+    width: "100%",
+    paddingLeft: "70%",
+  },
+  gear: {
+    height: "100%",
+    width: "15%",
+  },
+  logo: {
+    height: "30%",
+    width: "30%",
+  },
+  titleContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "1px solid green",
+    height: "100%",
+  },
+};
